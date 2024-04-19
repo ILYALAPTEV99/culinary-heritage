1f4f5296938e55c7cd76da10c0e1cc3883bd09ea
ILYALAPTEV99 <ilyalaptev001@gmail.com>
2024-04-19 17:42:27 +0300

    scroll_button"

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-u«Z
+let head_button_scroll = document.querySelector('.head_button_scroll')
+
+
+anime({
+    targets: '.head_button_scroll',
+    scale: [
+        { value: 1, duration: 700},
+        { value: 1.2, duration: 1100},
+        { value: 1, duration: 700},
+      ],
+    loop: true,
+    duration: 2500,
+    easing: 'linear'
+})
+
+let form = document.querySelector('.forma');   
+head_button_scroll.addEventListener('click', function () {
+  form.scrollIntoView({ 
+    block: 'start',
+    behavior: 'smooth',
+  });
+});
